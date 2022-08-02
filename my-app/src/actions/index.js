bc27714c1fcae329d34c2efb77dfc036598cdb16
Hayato Shimizu <haya12to17@gmail.com>
2022-08-02 19:23:44 +0900

    reducerの実装

---
+++ my-app/src/actions/index.js
@@ -0,0 +1,12 @@
+
+export const INCREMENT = 'INCREMENT'  //後でReducerで使うための記述
+export const DECREMENT = 'DECREMENT'  //後でReducerで使うための記述
+
+export const increment = () => ({
+    type: 'INCREMENT'
+})
+
+export const decrement = () => ({
+    type: 'DECREMENT'
+})
+
