8fa50db8d0d0cc8d801aed98daa32dadc69c722c
Hayato Shimizu <haya12to17@gmail.com>
2022-08-02 19:34:07 +0900

    storeの実装

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { creareStore } from 'redux'; //Storeを作成するための関数
+import { Provider } from 'react-redux'; //作成したStoreを全コンポーネントに渡すための特殊なコンポーネント
+
+import App from './components/App';
+import reducer from './reducer';
+
+const store = creareStore(reducer) //ここで作られるstoreはアプリケーション内部で唯一であり、アプリケーション内部の全てのStateはここに集約される。
+
+
+//どのコンポーネントからもstoreを参照できるようにするコンポーネントをProviderでラップする
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store= {store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
